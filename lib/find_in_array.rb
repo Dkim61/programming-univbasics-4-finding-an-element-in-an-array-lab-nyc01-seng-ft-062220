6801217edd8d3723ca95025b292615c70d5749b9
david <dkim187@binghamton.edu>
2020-06-16 03:34:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0 
   number_at_index = nil
     while count < array.length do
-      if array.count == value_to_find
+      if array[count] == value_to_find
         number_at_index = count
       end
         count += 1
